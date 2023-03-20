f148be520dd929c57ba3bdbddc4534988c0ce391
Jules <jcamuzet@yahoo.com>
2023-03-20 23:02:36 +0100

    basics

--- token.sol
+++ token.sol
@@ -13,8 +13,66 @@ contract chancla {
 
     mapping(address => mapping(address => uint256)) public _allowances;
 
+    event Approval(address owner, address spender, uint256 amount);
+    event Transfer(address from, address to, uint256 amount);
+
     constructor() {
         _balances[msg.sender] = _totalSupply;
+    }
+
+
+    // GETTERS
+    function totalSupply() external view returns(uint256) {
+        return _totalSupply;
+    }
+
+    function balanceOf(address account) external view returns(uint256) {
+        return _balances[account];
+    }
+
+    function allowance(address owner, address spender) external view returns(uint256) {
+        return _allowances[owner][spender];
+    }
+
+
+    // WRITERS 
+    function approve(address spender, uint256 amount) external returns(bool) {
+
+        require(msg.sender != address(0));
+        require(spender != address(0));
+
+        _allowances[msg.sender][spender] = amount;
+
+        return true;
+
+    }
+
+    function transfer(address to, uint256 amount) external returns(bool) {
+        
+        require(_balances[msg.sender] >= amount);
+
+        _balances[to] += amount;
+        _balances[msg.sender] = amount;
+
+        emit Transfer(msg.sender, to, amount);
+
+        return true;
+
+    }
+
+    function transferFrom(address from, address to, uint256 amount) external returns(bool) {
+
+        require(_balances[from] >= amount);
+        require(_allowances[from][msg.sender] >= amount);
+
+        _balances[from] -= amount;
+        _balances[to] += amount;
+        _allowances[from][msg.sender] -= amount;
+
+        emit Transfer(from, to, amount);
+
+        return true;
+
     }
 
 }
